bd2bb4ee0647b3a60c71180218f09b20e65280e0
Kelly de Vries & Brian Kropff <student@epicodus.com>
2015-04-08 10:31:29 -0700

    Make all coins appear in the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 function coinCombo(coins){
   var change = []
-
-  if (coins >= 25){
-    change.push("quarter");
-    coins -= 25;
-  }else if(coins >= 10) {
-    return "dime";
-  }else if(coins >= 5){
-    return "nickel";
-  }else if(coins > 0){
-    return "penny";
-  }else{
-    return false;
-  };
-
-
+  if (coins === 0){
+    change.push("no change for you");
+  }else {
+    while (coins>0){
+      if (coins >= 25){
+        change.push("quarter");
+        coins -= 25;
+      }else if(coins >= 10) {
+        change.push("dime");
+        coins -= 10;
+      }else if(coins >= 5){
+        change.push("nickel");
+        coins -= 5;
+      }else{
+        change.push("penny");
+        coins -=1;
+      }
+    }
+  }
 
   return change;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,18 @@
 describe('coinCombo', function(){
     it("will return false if a 0 is entered", function(){
-        expect(coinCombo(0)).to.equal(false);
+        expect(coinCombo(0)).to.eql(["no change for you"]);
     });
 
     it("will return a penny on a one", function(){
-        expect(coinCombo(1)).to.equal("penny");
+        expect(coinCombo(1)).to.eql(["penny"]);
     });
 
     it("will return nickel on a 5", function(){
-        expect(coinCombo(5)).to.equal("nickel");
+        expect(coinCombo(5)).to.eql(["nickel"]);
     });
 
     it("will return dime on a 10", function () {
-        expect(coinCombo(10)).to.equal("dime");
+        expect(coinCombo(10)).to.eql(["dime"]);
     });
 
     it("will return quarter on a 25", function() {
@@ -22,4 +22,8 @@ describe('coinCombo', function(){
     it("will return 2 quarters on 50", function() {
         expect(coinCombo(50)).to.eql(["quarter","quarter"]);
     });
+
+    // it("will return a quarter, a dime, a nickel, and a penny", function() {
+    //   expect(coinComber(50)).to.eql(["quarter",])
+    // })
 });
