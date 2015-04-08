b0a90037a975be9a98785a80a3c0abce3d0a91cb
Kelly de Vries & Brian Kropff <student@epicodus.com>
2015-04-08 12:03:50 -0700

    make all tests pass for one function, take off final space at end of result string, eliminated display of coins with zero value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,13 @@
 function coinCombo(coins){
-  var change = [];
-  if (coins === 0){
-    change.push("no change for you");
-  }else {
-    while (coins>0){
-      if (coins >= 25){
-        change.push("quarter");
-        coins -= 25;
-      }else if(coins >= 10) {
-        change.push("dime");
-        coins -= 10;
-      }else if(coins >= 5){
-        change.push("nickel");
-        coins -= 5;
-      }else{
-        change.push("penny");
-        coins -=1;
-      }
-    }
-  }
-
-  return change;
-};
-
-function coinCombo2(coins){
   var countQ = 0;
   var countD = 0;
   var countN = 0;
   var countP = 0;
+  var result = "";
+  var noChange = "No change for you! "
 
   if (coins === 0){
-    change.push("no change for you");
+    result = noChange;
   }else {
     while (coins>0){
       if (coins >= 25){
@@ -49,41 +26,6 @@ function coinCombo2(coins){
     }
   }
 
-  countQ = countQ + " " + "quarters ";
-  countD = countD + " " + "dimes ";
-  countN = countN + " " + "nickels ";
-  countP = countP + " " + "pennies";
-
-  var result = countQ + countD +countN + countP;
-  return result;
-};
-
-function coinCombo3(coins){
-  var countQ = 0;
-  var countD = 0;
-  var countN = 0;
-  var countP = 0;
-
-  if (coins === 0){
-    change.push("no change for you");
-  }else {
-    while (coins>0){
-      if (coins >= 25){
-        countQ += 1;
-        coins -= 25;
-      }else if(coins >= 10) {
-        countD += 1;
-        coins -= 10;
-      }else if(coins >= 5){
-        countN += 1;
-        coins -= 5;
-      }else{
-        countP += 1;
-        coins -=1;
-      }
-    }
-  }
-var result = "";
 
   if (countQ > 0){
     result += countQ + " " + "quarters ";
@@ -93,12 +35,76 @@ var result = "";
     result += countD + " " + "dimes ";
   }
   if (countN > 0){
-    var finalN = countN + " " + "nickels ";
+     result += countN + " " + "nickels ";
   }
   if(countP > 0){
-    var finalP = countP + " " + "pennies ";
+     result += countP + " " + "pennies ";
   }
   result = result.substring(0, result.length - 1);
   //var result = finalQ + finalD + finalN + finalP;
   return result;
 };
+
+
+
+
+
+// function coinCombo(coins){
+//   var change = [];
+//   if (coins === 0){
+//     change.push("no change for you");
+//   }else {
+//     while (coins>0){
+//       if (coins >= 25){
+//         change.push("quarter");
+//         coins -= 25;
+//       }else if(coins >= 10) {
+//         change.push("dime");
+//         coins -= 10;
+//       }else if(coins >= 5){
+//         change.push("nickel");
+//         coins -= 5;
+//       }else{
+//         change.push("penny");
+//         coins -=1;
+//       }
+//     }
+//   }
+//
+//   return change;
+// };
+//
+// function coinCombo2(coins){
+//   var countQ = 0;
+//   var countD = 0;
+//   var countN = 0;
+//   var countP = 0;
+//
+//   if (coins === 0){
+//     change.push("no change for you");
+//   }else {
+//     while (coins>0){
+//       if (coins >= 25){
+//         countQ += 1;
+//         coins -= 25;
+//       }else if(coins >= 10) {
+//         countD += 1;
+//         coins -= 10;
+//       }else if(coins >= 5){
+//         countN += 1;
+//         coins -= 5;
+//       }else{
+//         countP += 1;
+//         coins -=1;
+//       }
+//     }
+//   }
+//
+//   countQ = countQ + " " + "quarters ";
+//   countD = countD + " " + "dimes ";
+//   countN = countN + " " + "nickels ";
+//   countP = countP + " " + "pennies";
+//
+//   var result = countQ + countD +countN + countP;
+//   return result;
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,51 +1,49 @@
 describe('coinCombo', function(){
     it("will return false if a 0 is entered", function(){
-        expect(coinCombo(0)).to.eql(["no change for you"]);
+        expect(coinCombo(0)).to.equal("No change for you!");
     });
 
     it("will return a penny on a one", function(){
-        expect(coinCombo(1)).to.eql(["penny"]);
+        expect(coinCombo(4)).to.equal("4 pennies");
     });
 
     it("will return nickel on a 5", function(){
-        expect(coinCombo(5)).to.eql(["nickel"]);
+        expect(coinCombo(5)).to.equal("1 nickels");
     });
 
     it("will return dime on a 10", function () {
-        expect(coinCombo(10)).to.eql(["dime"]);
+        expect(coinCombo(10)).to.equal("1 dimes");
     });
 
     it("will return quarter on a 25", function() {
-        expect(coinCombo(25)).to.eql(["quarter"]);
+        expect(coinCombo(25)).to.equal("1 quarters");
     });
 
     it("will return 2 quarters on 50", function() {
-        expect(coinCombo(50)).to.eql(["quarter","quarter"]);
+        expect(coinCombo(50)).to.equal("2 quarters");
     });
 
     it("will return a quarter, a dime, a nickel, and a penny", function() {
-      expect(coinCombo(41)).to.eql(["quarter", "dime", "nickel", "penny"]);
+      expect(coinCombo(41)).to.equal("1 quarters 1 dimes 1 nickels 1 pennies");
     });
 
     it("will return a quarter, 2 dimes, and a penny", function() {
-      expect(coinCombo(46)).to.eql(["quarter", "dime", "dime", "penny"]);
+      expect(coinCombo(46)).to.equal("1 quarters 2 dimes 1 pennies");
     });
 
-    //coinCombo2
     it("will return '2 quarters' on 50", function(){
-      expect(coinCombo2(50)).to.eql("2 quarters 0 dimes 0 nickels 0 pennies");
+      expect(coinCombo(50)).to.equal("2 quarters");
     });
 
     it("will return '2 quarters 2 dimes' on 70", function(){
-      expect(coinCombo2(70)).to.equal("2 quarters 2 dimes 0 nickels 0 pennies");
+      expect(coinCombo(70)).to.equal("2 quarters 2 dimes");
     });
 
     it("will return '1 quarters 1 dimes 1 nickels 1 pennies' on 41", function(){
-      expect(coinCombo2(41)).to.equal("1 quarters 1 dimes 1 nickels 1 pennies");
+      expect(coinCombo(41)).to.equal("1 quarters 1 dimes 1 nickels 1 pennies");
     });
 
-    //coinCombo3
     it("will return only the coins that will be given (2 quarters 2 dimes)", function(){
-      expect(coinCombo3(70)).to.equal("2 quarters 2 dimes");
+      expect(coinCombo(70)).to.equal("2 quarters 2 dimes");
     })
 });
